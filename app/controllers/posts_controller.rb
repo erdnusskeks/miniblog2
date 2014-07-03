83a88bf89df1929fdb387246f406955d769ecb46
erdnusskeks <erdnusskeks@googlemail.com>
2014-07-03 20:14:35 +0200

    Add form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,28 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
 
 end
